de0ac15d8e861eb35ac95826a009b6aa9c1ea5c1
Asen Georgiev <asen.connor@abv.bg>
2017-07-05 19:20:29 +0300

    forgot this

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            var line = Console.ReadLine().Split(' ');
+            Console.WriteLine(new Student(line[0], line[1], line[2]).ToString());
+            line = Console.ReadLine().Split(' ');
+            Console.WriteLine(new Worker(line[0], line[1], double.Parse(line[2]), double.Parse(line[3])));
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+    }
+}
